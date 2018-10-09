2abe80792094dfb6f69877e92ff8605d143bd410
AJ Pant <anav.pant@gmail.com>
2018-10-08 21:35:51 -0400

    fix life cycle events + ajax

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,16 @@
-var React = require('react');
-var Popular = require('./Popular');
+var React = require("react");
+var Popular = require("./Popular");
 
-
-
-class App extends React.Component{
-    render(){
-        return (<div className='container'> //use className NOT class bc class is keyword in js
-            <Popular />
-        </div>);
-    }
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        {" "}
+        //use className NOT class bc class is keyword in js
+        <Popular />
+      </div>
+    );
+  }
 }
 
 //export App component so we can require in the main module
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,11 +1,15 @@
-var axios = require('axios');
+var axios = require("axios");
 
 module.exports = {
-    fetchPopularRepos: function(language){
-        var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+  fetchPopularRepos: function(language) {
+    var encodedURI = window.encodeURI(
+      "https://api.github.com/search/repositories?q=stars:>1+language:" +
+        language +
+        "&sort=stars&order=desc&type=Repositories"
+    );
 
-        return axios.get(encodedURI).then(function(response){
-              return response.data.items;
-        })
-    }
-}
+    return axios.get(encodedURI).then(function(response) {
+      return response.data.items;
+    });
+  }
+};
